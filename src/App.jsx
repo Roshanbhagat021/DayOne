9649f9a1b6264a57158afae65d1b0999b1822411
Roshan <roshanbhagatwork@gmail.com>
2025-04-05 19:47:11 +0530

    Completed the page4

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./Components/Header";
 import Page1 from "./Components/Page1";
 import Page2 from "./Components/Page2";
 import Page3 from "./Components/Page3";
+import Page4 from "./Components/Page4";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Page1 />
       <Page2/>
       <Page3/>
+      <Page4/>
     </>
   );
 }
---
+++ src/Components/Page4.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import AppStore from "../assets/app-store.svg"
+import GooglePlay from "../assets/google-play.svg"
+import MacStore from "../assets/mac-app-store.svg"
+import MicroSoftStore from "../assets/microsoft-store.png"
+import WebApp from "../assets/web-app.svg"
+
+export default function Page4() {
+  return (
+    <div className=" text-center bg-gradient-to-b from-[#f1f4f6] to-[rgba(241,244,246,0)] text-[#333b40] max-w-[95%] p-8 mx-auto mt-4">
+      <h1 className="text-4xl mb-8 ">
+        Capture life’s moments, anytime, anywhere.
+      </h1>
+      <p className=" text-lg/8 mt-4 max-w-[900px] text-left m-auto '">
+        Download the free Day One journal app for free on iPhone, Android, iPad,
+        Mac, and Apple Watch. Or access your Day One Journal from any browser.
+      </p>
+      <div className=" flex gap-4 justify-center flex-wrap items-center my-8">
+        <img className="w-[100%] max-w-[120px]" src={AppStore} alt="" />
+        <img className="w-[100%] max-w-[130px]" src={GooglePlay} alt="" />
+        <img className="w-[100%] max-w-[145px]" src={MacStore} alt="" />
+        <img className="w-[100%] max-w-[120px]" src={WebApp} alt="" />
+      </div>
+    </div>
+  );
+}
